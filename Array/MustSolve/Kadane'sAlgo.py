016e5c6fa2336624073d5f70e68ce1d3d7a43077
ReyathBegam <reyath.begam@gmail.com>
2026-06-22 22:59:55 +0530

    Learned Kadane Algorithm

---
+++ Array/MustSolve/Kadane'sAlgo.py
@@ -0,0 +1,31 @@
+# You are given an integer array arr[]. You need to find the maximum sum of a subarray (containing at least one element) in the array arr[].
+
+# Note : A subarray is a continuous part of an array.
+
+# Examples:
+
+# Input: arr[] = [2, 3, -8, 7, -1, 2, 3]
+# Output: 11
+# Explanation: The subarray [7, -1, 2, 3] has the largest sum 11.
+# Input: arr[] = [-2, -4]
+# Output: -2
+# Explanation: The subarray [-2] has the largest sum -2.
+# Input: arr[] = [5, 4, 1, 7, 8]
+# Output: 25
+# Explanation: The subarray [5, 4, 1, 7, 8] has the largest sum 25.
+# Constraints:
+# 1 ≤ arr.size() ≤ 105
+# -104 ≤ arr[i] ≤ 104
+
+class maximumSum():
+    def maxSubarraySum(self, arr):
+        # Code here
+        curr_sum=0
+        Max_sum=float("-inf")
+        for nums in arr:
+            curr_sum+=nums
+            Max_sum=max(curr_sum,Max_sum)
+            if curr_sum<0:
+                curr_sum=0
+        return Max_sum
+print(maximumSum().maxSubarraySum([2, 3, -8, 7, -1, 2, 3]))
